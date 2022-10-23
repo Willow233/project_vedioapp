f993f644741d22a86f658c50993b5b45e6b5d943
Willow233 <1642974246@qq.com>
2022-10-23 20:29:22 +0800

    first commit

---
+++ src/views/Home/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>主页</h1>
+    <router-link to="/video/1">点击进入视频详情页面</router-link>
+</template>
---
+++ src/views/Video/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>视频详情页</h1>
+    <router-link to="/">返回主页</router-link>
+</template>
